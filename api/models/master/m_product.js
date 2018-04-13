9c97b92762b9329426dd047613db517151443e42
antoniopratama <antoniopratama123@gmail.com>
2018-04-13 18:14:41 +0700

    product model tanggalnya

--- api/models/master/m_product.js
+++ api/models/master/m_product.js
@@ -7,9 +7,9 @@ const productSchema = mongoose.Schema({
     description: {type: String, required: false, trim: true},
     isDelete: {type: Boolean, required: true, default: false},
     createdBy: {type: String, required: true, trim: true},
-    createdDate: {type: Date, required: true, trim: true, default: Date.now()},
+    createdDate: {type: Date, required: true, default: Date.now()},
     updatedBy: {type: String, required: false, trim: true},
-    updatedDate: {type: Date, required: false, trim: true}
+    updatedDate: {type: Date, required: false}
 })
 
 module.exports = mongoose.model('Product', productSchema, 'products');
