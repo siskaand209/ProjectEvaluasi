5468a2b42fbcd57bff5dbe00afc31d8ada49f8e5
sptclr <sipitacular@gmail.com>
2018-04-16 11:14:33 +0700

    ref MenuAccess

--- api/models/master/m_menu_access.js
+++ api/models/master/m_menu_access.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const menuAccessSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    mRoleId: { type: String, required: true },
-    mMenuId: { type: String, required: true },
+    mRoleId: { type: mongoose.Schema.Types.ObjectId, ref: 'Role', required: true },
+    mMenuId: { type: mongoose.Schema.Types.ObjectId, ref: 'Menu', required: true },
     isDelete: { type: Boolean, required: true, default: false },
     createdBy: { type: String, required: true, trim: true, },
     updatedBy: { type: String, required: false, trim: true },
