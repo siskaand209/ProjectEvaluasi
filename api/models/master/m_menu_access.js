399969d2d900ab8a4c8b22cf997bd87c8a7ef6be
sptclr <sipitacular@gmail.com>
2018-04-16 17:18:31 +0700

    Updated Menu Access Model

--- api/models/master/m_menu_access.js
+++ api/models/master/m_menu_access.js
@@ -5,7 +5,7 @@ const menuAccessSchema = mongoose.Schema({
     role: { type: mongoose.Schema.Types.ObjectId, ref: 'Role', required: true },
     menu: { type: mongoose.Schema.Types.ObjectId, ref: 'Menu', required: true },
     isDelete: { type: Boolean, required: true, default: false },
-    createdBy: { type: String, required: true, trim: true, },
+    createdBy: { type: String, required: false, trim: true, },
     updatedBy: { type: String, required: false, trim: true },
 },
 {timestamps: { createdAt: 'createdDate', updatedAt: 'updatedDate' }}
