c093ad165cb60196b11f22cf6264f16ea8e216c2
qalfin <qalfinq@gmail.com>
2018-04-19 11:13:55 +0700

    populate user

--- app.js
+++ app.js
@@ -10,13 +10,12 @@ const productRoutes = require('./api/routes/master/m_products');
 const menuRoutes = require('./api/routes/master/m_menus');
 const roleRoutes = require('./api/routes/master/m_roles');
 const employeeRoutes = require('./api/routes/master/m_employees');
-const menuAccessRoutes = require('./api/routes/master/m_menuaccesses');
-const companiesRoutes = require('./api/routes/master/m_companies')
+const menuAccessRoutes = require('./api/routes/master/m_menuaccesses')
 
 //conect to mongo
 //password admin01
-// mongoose.connect("mongodb://localhost:27017/ProjectEvaluasiAPI");
-mongoose.connect("mongodb://admin01:admin01@ds247439.mlab.com:47439/projectevaluasi");
+ mongoose.connect("mongodb://localhost:27017/ProjectEvaluasiAPI");
+//mongoose.connect("mongodb://admin01:admin01@ds247439.mlab.com:47439/projectevaluasi");
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.urlencoded({extended:false}));
@@ -40,7 +39,6 @@ app.use('/api/menus', menuRoutes);
 app.use('/api/roles', roleRoutes);
 app.use('/api/employees', employeeRoutes);
 app.use('/api/menuaccesses', menuAccessRoutes);
-app.use('/api/companies', companiesRoutes);
 
 
 app.use((req, res, next)=>{
