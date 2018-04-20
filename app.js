69bc4a61f735ffc3ec7c01590afa614ca4963510
fajriashr <fajrisukmaashr@gmail.com>
2018-04-20 10:51:37 +0700

    appjs tambah appuse company

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use('/api/menus', menuRoutes);
 app.use('/api/roles', roleRoutes);
 app.use('/api/employees', employeeRoutes);
 app.use('/api/menuaccesses', menuAccessRoutes);
+app.use('/api/companies', companiesRoutes);
 
 
 app.use((req, res, next)=>{
